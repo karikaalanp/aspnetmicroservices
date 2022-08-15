ccae1bad3e1ae3b98e77f8794ea2ae8174a13f0f
Karikalan P <karikaalanp@outlook.com>
2022-08-15 19:19:09 +0530

    checking in after basket catalog and discount services

---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,24 @@
+﻿using Discount.Grpc.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcService
+    {
+        private readonly Discount.Grpc.Protos.Discount.DiscountClient _discountClient;
+
+        public DiscountGrpcService(Discount.Grpc.Protos.Discount.DiscountClient discountClient)
+        {
+            _discountClient = discountClient ?? throw new ArgumentNullException(nameof(discountClient));
+        }
+
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var discountRequest = new Discount.Grpc.Protos.GetDiscountRequest {ProductName = productName };
+            return await _discountClient.GetDiscountAsync(discountRequest);
+        }
+    }
+}
